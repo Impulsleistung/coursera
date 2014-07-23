808c6b2e9e0ef4015a063d369761daf5e5f88caa
Kevin Ostheimer <info@impulsleistung.de>
2014-07-23 20:57:04 +0200

    Teil pollutant bestanden

--- 02_rProgramming/1_Assignment/pollutantmean.R
+++ 02_rProgramming/1_Assignment/pollutantmean.R
@@ -1,14 +1,42 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-  ## 'directory' is a character vector of length 1 indicating
-  ## the location of the CSV files
   
-  ## 'pollutant' is a character vector of length 1 indicating
-  ## the name of the pollutant for which we will calculate the
-  ## mean; either "sulfate" or "nitrate".
+  # Argumente
+  x_path<-directory
+  x_customRange<-id
+  x_pollutant<-pollutant
   
-  ## 'id' is an integer vector indicating the monitor ID numbers
-  ## to be used
+  #x_path<-"specdata"
+  #x_customRange<-c(70:72)
+  #x_pollutant<-"nitrate"
   
-  ## Return the mean of the pollutant across all monitors list
-  ## in the 'id' vector (ignoring NA values)
+  # Definitions
+  setwd(dir = "/home/impulsleistung/Dokumente/coursera_git//02_rProgramming//1_Assignment")
+  x_pwd<-getwd()
+  
+  # Alle Dateien lokalisieren
+  x_files<-dir(path = x_path,recursive = FALSE)
+  x_files<-list(x_files)
+  
+  x_selectedFiles<-x_files[[1]][x_customRange]
+  
+  ## Von jedem File die Matrix
+  for(i in x_selectedFiles) {
+    x_targetFile<-paste(x_path,i,sep = "/")  
+    dat<-read.csv(file = x_targetFile,header = TRUE,sep = ",",quote = "\"")
+    if(!exists("erg_mat")) {
+      erg_mat<-dat
+    }
+    else {
+      erg_mat<-rbind(erg_mat,dat)
+    }
+  }
+  
+  erg_frame<-data.frame(erg_mat)
+  # Achtung, gefordert wird aber nur der MEANwert von Sulfat oder Nitrat
+  x_pol_vec<-with(erg_frame,get(x_pollutant))
+  rm("dat", "erg_frame", "erg_mat")
+  # In der letzten Zeile steht das Endergebnis
+  x_resultNR<-mean(x = x_pol_vec,na.rm = TRUE)
+  round(x_resultNR,digits = 3)
+  
 }
