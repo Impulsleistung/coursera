97c45b23ba029a23dec4ef9064d1b141e1c49dc1
Kevin Ostheimer <info@impulsleistung.de>
2014-07-27 15:45:35 +0200

    Assignment 2 stable

--- 02_rProgramming/01_library_clean.R
+++ 02_rProgramming/01_library_clean.R
@@ -18,3 +18,58 @@ colMeans(x = dat_sub)
 dat_sub <- subset(x = good_dat, subset = good_dat$Month==5)
 max(dat_sub$Ozone)
 
+# Statische Funktionen mit Speicherobjekten
+make.f <- function() {
+  count <- 0 # Speichervariable
+  # Wenn die Funktion instanziert wurde ist "x" der Übergabeparameter
+  f <- function(x) {
+    count <<- count + 1 # Die statische Variable des übergeordneten Frame schreiben
+    return( list(mean=mean(x), count=count) ) # Das Rückgabeobjekt geht in den übergeordneten Frame
+  }
+  return( f ) # Erst hier geht das Rückgabeobjekt in den Workspace
+}
+
+f1<-make.f()
+result<-f1(1:10)
+result<-f1(1:10)
+
+# Dieser Prototyp hat Konstruktorvariablen
+# Dies ist aber nur eine intelligente Datenstruktur
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  # Die Konstruktorvariable x und die Variable m sind statisch
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() 
+    x
+  # Diese Funktion wird von aussen Aufgerufen
+  setmean <- function(mean) 
+    m <<- mean
+  getmean <- function() 
+    m
+  # Damit die Funktionen wie Variablen im Frame ansprechbar sind wird eine Liste generiert
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
+# Die Realisierung und Adressierung ist wie folgt
+x<-makeVector(1:10)
+x$get()
+x$set(1:5)
+
+
+# Die obige Datenstruktur enthält die abstrakten Funktionen $setmean und $getmean
+# Die folgende Funktion wendet Routinen auf die statischen Variable m an
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+}
--- 02_rProgramming/1_Assignment/testvector.R
+++ 02_rProgramming/1_Assignment/testvector.R
@@ -1,7 +1,46 @@
-rm(list=ls())
-setwd(dir = "/home/impulsleistung/Dokumente/coursera_git//02_rProgramming//1_Assignment")
-source("corr.R")
-source("complete.R")
-cr <- corr("specdata",2000)
-summary(cr)
-head(cr)
+# By Kevin Ostheimer
+
+# This function creates a special "matrix" object that can cache its inverse
+makeCacheMatrix <- function(x_mat = matrix()) {
+  m_inverse <- NULL
+  # The constructor x_mat and m_inverse are static
+  set <- function(y_mat) {
+    x_mat <<- y_mat
+    m_inverse <<- NULL
+  }
+  get <- function() x_mat
+  # This function is called from an upper frame
+  setInverse <- function(inverse) m_inverse <<- inverse
+  getInverse <- function() m_inverse
+  # The internal functions are pointed to listvariables
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+}
+
+# This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+# If the inverse has already been calculated
+cacheSolve <- function(mat_norm) {
+  ## Return a matrix that is the inverse of 'mat_norm'
+  m <- mat_norm$getInverse()
+  # If is not empty, the inverse is already calculated...
+  if(!is.null(m)) {
+    message("getting cached data of inverse matrix")
+    return(m)
+  }
+  # Else m is empty, the calculation routine comes here
+  data <- mat_norm$get()
+  m <- solve(data)
+  # Setting the result to the frame of the argument mat_norm
+  mat_norm$setInverse(m)
+  m # Display the result
+}
+
+###############################################################
+## Testprocedure for Programming Assignment 2: Lexical Scoping
+## Feel free to uncomment ;-)
+###############################################################
+# zzz<-matrix(rnorm(n = 9,mean = 10,sd = 2),nrow = 3,ncol = 3)
+# aaa<-makeCacheMatrix(zzz)
+# cacheSolve(aaa)
+# cacheSolve(aaa) # This is the proof
